de504875d583d02fc2de3c3195bba43a53d61216
zacktcheng <zacktcheng@gmail.com>
2023-04-06 02:25:33 -0700

    3 commit

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     proxy: {
@@ -10,4 +8,4 @@ module.exports = defineConfig({
       }
     }
   }
-})
+}
